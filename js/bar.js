4260ead78cd16aafd273ef885b4db6b1f064e508
Dina Konto <dina.konto@gmail.com>
2020-08-12 12:25:55 +1000

    Refactored bar to calculate % instead of hardcoded

--- js/bar.js
+++ js/bar.js
@@ -1,7 +1,12 @@
-const barGrad = $("#bar")
-const barSpot = $("#bar-dot")
+const barGrad = $("#bar");
+const barSpot = $("#bar-dot");
+
+const gbTotal = 1000;
+let gbUsed = 815;
+let gbLeft = gbTotal - gbUsed;
+let pcUsed = (gbUsed / gbTotal) * 100;
 
 $(function() {
-  barGrad.animate({ width: '80%' }, 500);
-  barSpot.animate({left: '78%'}, 500);
+  barGrad.animate({ width: pcUsed + '%' }, 500);
+  barSpot.animate({ right: '2px'}, 500);
 })
